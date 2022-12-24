225cf2272125be9f56ce0f41e8bfc932e039133f
jack-of-all-trades-22 <adi27184@gmail.com>
2022-12-24 15:07:43 +0530

    Testimonials updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
-import Hero from './components/Hero'
-import Reason from './components/Reasons/Reason';
+import "./App.css";
+import Hero from "./components/Hero";
+import Join from "./components/join/join";
+import Reason from "./components/Reasons/Reason";
+import Testimonials from "./components/Testimonials/Testimonials";
 function App() {
   return (
     <div className="App">
       <Hero />
       <Reason />
+      <Testimonials />
+      <Join />
     </div>
   );
 }
