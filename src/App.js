5ab2b6fb72b79bc525bf73feef534e2bb958a45c
S-15-77 <santhoshdayakar2002@gmail.com>
2022-12-24 14:43:10 +0530

    sd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Hero from './components/Hero'
+import { Programs } from './Programs/Programs';
 function App() {
   return (
     <div className="App">
       <Hero />
+      <Programs/>
     </div>
   );
 }
