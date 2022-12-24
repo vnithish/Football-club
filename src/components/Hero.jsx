99c2e64f1ef0f2054328398b444e81305b9e8f6c
nithish <vnithish12@gmail.com>
2022-12-24 13:01:57 +0530

    hero section done animations added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import Header from "./Header/Header";
 import './Hero.css' ;
+import hero_image from "../assets/hero_image.png";
+import hero_image_back from "../assets/hero_image_back.png";
+import Heart from "../assets/heart.png";
+import Calories from "../assets/calories.png";
 
+
+import {motion} from 'framer-motion'
 const Hero = () => {
+  const transition = {type: 'spring', duration:3}
   return (
     <div className="hero">
       <div className="left-h">
         <Header />
 
         <div className="the-best-ad">
-          <div></div>
+          <motion.div
+            initial={{left: '238px'}}
+            whileInView={{left: '8px'}}
+            transition={{...transition, type: 'tween'}}
+          ></motion.div>
           <span>The Best Football Club In Town</span>
         </div>
 
@@ -26,9 +37,59 @@ const Hero = () => {
           In here we will help you level up your soccer skills.
           </span>
         </div>
+      
+
+      {/*figures*/}
+      <div className="figures">
+        <div>
+          <span>+5</span>
+          <span>Coaches</span>
+        </div>
+        <div>
+          <span>+500</span>
+          <span>Members Joined</span>
+        </div>
+        <div>
+        <span>+10</span>
+        <span>Coaching Programs</span>
+        </div>
       </div>
+
+      <div className="hero-buttons">
+        <buttons className="btn">Get Started</buttons>
+        <buttons className="btn">Learn More</buttons>
+      </div>
+    </div>
       <div className="right-h">
-        Right Side
+        <button className="btn">Join Now</button>
+
+        <motion.div 
+        className="heart-rate"
+        transition={transition}
+        whileInView={{right: "4rem"}}
+        initial={{right: "-1rem"}}>
+          <img src={Heart} alt="" />
+          <span>Heart Rate</span>
+          <span>116 bpm</span>
+        </motion.div>
+
+        <img src={hero_image} alt="" className="hero-image" />
+        <motion.img 
+        initial={{right: "11rem"}}
+        whileInView={{right: "20rem"}}
+        transition={transition}
+        src={hero_image_back} alt="" className="hero-image-back" />
+
+        <motion.div className="goals"
+        transition={transition}
+        whileInView={{right: "35rem"}}
+        initial={{right: "37rem"}}>
+          <img src={Calories} alt="" />
+          <div>
+            <span>Goals Scored</span>
+            <span>10+</span>
+          </div>
+        </motion.div>
       </div>
     </div>
   )
