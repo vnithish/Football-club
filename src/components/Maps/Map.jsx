b33e071a6fc29d46d199a3bf13e8914c943927eb
Aakash <56353087+xii-aakash@users.noreply.github.com>
2023-01-06 11:51:25 +0530

    footer responsiveness

--- src/components/Maps/Map.jsx
+++ src/components/Maps/Map.jsx
@@ -3,7 +3,7 @@ import './Map.css'
 function Map() {
   return (
     <div className='maps'>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15560.446529365328!2d80.2295172!3d12.8360623!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x1a52dc596fb1e447!2sDugout%20Trampoline%20and%20Sports%20Arena!5e0!3m2!1sen!2sin!4v1671960477311!5m2!1sen!2sin" width="560px" height="560px" style={{border:0, borderRadius: 30, }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15560.446529365328!2d80.2295172!3d12.8360623!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x1a52dc596fb1e447!2sDugout%20Trampoline%20and%20Sports%20Arena!5e0!3m2!1sen!2sin!4v1671960477311!5m2!1sen!2sin" style={{border:0, borderRadius: 30, }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
   )
 }
