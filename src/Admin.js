bc3b3c73711195aea66cf10504518f11065d1cf1
Aakash <56353087+xii-aakash@users.noreply.github.com>
2023-01-07 15:46:54 +0530

    admin page css/responsiveness added

--- src/Admin.js
+++ src/Admin.js
@@ -1,42 +1,50 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import { storage } from './firebase';
-import { ref, uploadBytes, listAll , getDownloadURL} from 'firebase/storage';
-import {v4} from 'uuid'
+import { ref, uploadBytes, listAll, getDownloadURL } from 'firebase/storage';
+import { v4 } from 'uuid'
+import './Admin.css'
+import Button from '@mui/material/Button';
+import frame from "./assets/logo.png";
 
 function Admin() {
-  const [imageUpload , setImageUpload] = useState(null)
-  const [imageList , setImageList] = useState([]);
-  const imageListRef = ref(storage , "images/")
-  const uploadImage = () =>{
-    if (imageUpload == null) return ;
-    const imageRef = ref(storage , `images/${imageUpload.name + v4()}`);
+  const [imageUpload, setImageUpload] = useState(null)
+  const [imageList, setImageList] = useState([]);
+  const imageListRef = ref(storage, "images/")
+  const uploadImage = () => {
+    if (imageUpload == null) return;
+    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
     uploadBytes(imageRef, imageUpload).then((snapshot) => {
-      // alert("Image Uploaded")
+      alert("Image Uploaded")
       getDownloadURL(snapshot.ref).then((url) => {
-        setImageList((prev) => [...prev,url])
+        setImageList((prev) => [...prev, url])
       })
-      
+
     });
 
-    
+
   }
-  useEffect(() =>{
-    listAll(imageListRef).then((response) =>{
+  useEffect(() => {
+    listAll(imageListRef).then((response) => {
       response.items.forEach((item) => {
         getDownloadURL(item).then((url) => {
-          setImageList((prev) => [...prev,url]);
+          setImageList((prev) => [...prev, url]);
         })
       })
     })
-  },[])
+  }, [])
   return (
-    <div className="App">
-      <input type="file" onChange={(event) => {setImageUpload(event.target.files[0])}}/>
-      <button onClick={uploadImage}>Upload Image</button>
-      {imageList.map((url) => {
+    <div className='container'>
+      <div className='admin'>
+        <img src={frame} />
+        <h3>Pro Soccer School <br/><span>Admin Access</span></h3>
+        <input type="file" onChange={(event) => { setImageUpload(event.target.files[0]) }} />
+        {/* <button onClick={uploadImage}>Upload Image</button> */}
+        <Button className='btn' onClick={uploadImage}>Upload</Button>
+        {/* {imageList.map((url) => {
         return <img src= {url} alt="" />
-      })}
+      })} */}
+      </div>
     </div>
   );
 }
