3100d05b6f24eb9f0b09d469edb4eddf59f28683
Aakash <56353087+xii-aakash@users.noreply.github.com>
2023-01-07 19:47:50 +0530

    contact us form fixed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,16 @@
 import React, { useRef } from "react";
 import Map from "../Maps/Map";
 import "./Footer.css";
+import {useState} from 'react';
 
 
 import emailjs from "@emailjs/browser";
 
 function Footer() {
   const form = useRef();
+  const [nameText , setName] = useState("");
+  const [phnText, setPhn] = useState("");
+  const [msgText, setMsg] = useState("");
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -26,6 +30,11 @@ function Footer() {
           console.log(error.text);
         }
       );
+
+      setMsg("");
+      setName("");
+      setPhn("");
+
   };
   return (
     <div className="footer">
@@ -36,12 +45,30 @@ function Footer() {
         <form ref={form} onSubmit={sendEmail}>
           <p>Contact Us</p>
           <label>Name</label>
-          <input type="text" name="user_name" />
+          <input
+            type="text"
+            name="user_name"
+            value={nameText}
+            onChange={(e) => setName(e.target.value)}
+          />
           <label>Contact Number</label>
-          <input type="text" name="user_number" />
+          <input
+            type="text"
+            name="user_number"
+            value={phnText}
+            onChange={(e) => setPhn(e.target.value)}
+          />
           <label>Message</label>
-          <textarea name="message" />
-          <input type="submit" value="Submit" style={{backgroundColor:'black', color:'white'}}/>
+          <textarea
+            name="message"
+            value={msgText}
+            onChange={(e) => setMsg(e.target.value)}
+          />
+          <input
+            type="submit"
+            value="Submit"
+            style={{ backgroundColor: "black", color: "white" }}
+          />
         </form>
       </div>
     </div>
