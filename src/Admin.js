934a1519074c22eeb9378d6ca28a97442a579d31
S-15-77 <santhoshdayakar2002@gmail.com>
2023-01-07 14:48:57 +0530

    added image uploader in admin page

--- src/Admin.js
+++ src/Admin.js
@@ -1,9 +1,44 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import { storage } from './firebase';
+import { ref, uploadBytes, listAll , getDownloadURL} from 'firebase/storage';
+import {v4} from 'uuid'
 
 function Admin() {
+  const [imageUpload , setImageUpload] = useState(null)
+  const [imageList , setImageList] = useState([]);
+  const imageListRef = ref(storage , "images/")
+  const uploadImage = () =>{
+    if (imageUpload == null) return ;
+    const imageRef = ref(storage , `images/${imageUpload.name + v4()}`);
+    uploadBytes(imageRef, imageUpload).then((snapshot) => {
+      // alert("Image Uploaded")
+      getDownloadURL(snapshot.ref).then((url) => {
+        setImageList((prev) => [...prev,url])
+      })
+      
+    });
+
+    
+  }
+  useEffect(() =>{
+    listAll(imageListRef).then((response) =>{
+      response.items.forEach((item) => {
+        getDownloadURL(item).then((url) => {
+          setImageList((prev) => [...prev,url]);
+        })
+      })
+    })
+  },[])
   return (
-    <div>Admin</div>
-  )
+    <div className="App">
+      <input type="file" onChange={(event) => {setImageUpload(event.target.files[0])}}/>
+      <button onClick={uploadImage}>Upload Image</button>
+      {imageList.map((url) => {
+        return <img src= {url} alt="" />
+      })}
+    </div>
+  );
 }
 
 export default Admin
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import {getStorage} from 'firebase/storage'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,6 +18,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+export const storage = getStorage(app)
 
 export { auth };
 export default app;
